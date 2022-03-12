5605d4edee9c5a18eba951366a2b8b4ff78780d6
Abimanyu003 <abimanyn@codincity>
2022-03-12 12:49:48 +0000

    third commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hi Abimanyu\n'
+  const msg = 'Hi welcome to codincity\n'
   res.end(msg);
 });
 
